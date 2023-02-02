16b835c44d5cd522328df6972193784c02421144
KashyapAmanSingh <coder85113@gmail.com>
2023-02-02 23:17:04 +0530

    moduleer_import ful  concept

--- imprt.js
+++ imprt.js
@@ -1,12 +1,17 @@
-import { msg , helo, test } from "./prac";
+// import { msg , helo, test } from "./prac";
 // take short name of exported item for impoort by their short name 
 // import { msg  as m , helo as h, test as t } from "./prac";
 // for print shotcut name    -----------------   console.log(m);
 
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 // but abhi bhe hame baar bar expoerted file ko import karnay kay liye uska name use karna padta hai .If We Use universal selector * then it became easy 
-import  * as yahoo from "./prac";   // now by only using yahoo we can use all exported file like this 
+// import  * as yahoo from "./prac";   // now by only using yahoo we can use all exported file like this 
 // console.log(yahoo.msg);       console.log(universal_import_name.exported-filenaem)
 
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//   import defalut as yahoo 
 
 
 
